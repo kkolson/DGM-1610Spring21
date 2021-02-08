be558b25625917dd01854d261675c021e0c1286f
Katelynn <10828878@my.uvu.edu>
2021-02-08 15:12:48 -0700

    Variables and Operators
    C# variable/operator practice #1

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 7;
+            b = a;
+            c = b++;
+            b = a + b * c;
+            c = a >= 100 ? b : c / 10;
+            a = (int) Math.Sqrt(b * b + c * c);
+
+            string s = "String literal";
+            char l = s[s.Length - 1];
+
+            var numbers = new List<int>(new[] {1, 2, 3});
+            b = numbers.FindLast(n => n > 1);
+        }
+    }
+}

c93368123eb1b5dd8ce292ccaaef45cae8d45f7f
Katelynn <10828878@my.uvu.edu>
2021-02-08 10:51:11 -0700

    BB8 Game
    Started counter script

---
+++ UVU Truck Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public int creditValue = 3;
+    public int numberCount;
+    public Text counterText;
+    
+    void OnTriggerEnter2d()
+    {
+        numberCount += creditValue;
+        counterText.text = numberCount.ToString();
+        gameObject.SetActive(false);
+    }
+}

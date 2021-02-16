248011519206193123ca7a87069916acbc33a73f
Katelynn <10828878@my.uvu.edu>
2021-02-15 23:19:00 -0700

    Loops
    Simple loop practice

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                Console.WriteLine("Don't try it Anakin, I have the high ground!");
+            }
+                
+        }
+    }
+}

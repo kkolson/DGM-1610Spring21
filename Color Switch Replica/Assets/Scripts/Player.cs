8567bf43bfcafcf231830111f79963daa2ccbda1
Katelynn <10828878@my.uvu.edu>
2021-02-17 16:18:48 -0700

    Color Switch
    Added Main player and controls

---
+++ Color Switch Replica/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        
+    }
+}

4efe10dafcb799ca314970ecc68c03d1d7fd5c81
Katelynn <10828878@my.uvu.edu>
2021-02-17 15:13:53 -0700

    IfStatements
    if statements practice

---
+++ IfStatements/IfStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20, y = 10;
+            if (x >= 10)
+            {
+                Console.WriteLine("x is Greater than 10");
+            }
+
+            if (y >= 5)
+            {
+                Console.WriteLine("y is less than or equals to 5");
+            }
+
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}

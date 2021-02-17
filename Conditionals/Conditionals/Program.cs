3d6b9c29d5dd75298502aeaf91cd39173f2566d1
Katelynn <10828878@my.uvu.edu>
2021-02-17 15:07:44 -0700

    Conditionals
    Practice Script

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int time = 22;
+if (time < 10)
+{
+    Console.WriteLine("Good Morning.");
+}
+else if (time < 20)
+{
+    Console.WriteLine("Good day.");
+}
+else
+{
+    Console.WriteLine("Good evening.");
+}
+// Outputs "Good evening 

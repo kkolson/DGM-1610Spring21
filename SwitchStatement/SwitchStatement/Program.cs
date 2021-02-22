5454fd17cbdcbe278fe1dfd0ec4f5c7845c92727
Katelynn <10828878@my.uvu.edu>
2021-02-21 19:29:26 -0700

    Switch Statement
    Switch Practice

---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            switch (x)
+            {
+                case 10:
+                    Console.WriteLine("x value is 10");
+                    break;
+                case 15:
+                    Console.WriteLine("x value is 15");
+                    break;
+                case 20:
+                    Console.WriteLine("x value is 20");
+                    break;
+                default:
+                    Console.WriteLine("Not Known");
+                    break;
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}

74b442ad86148ed0d193f310bae1f6bfa0a89643
Katelynn <10828878@my.uvu.edu>
2021-02-22 20:21:44 -0700

    ForLoop
    For loop practice #1

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = i; j < 3; j++)
+                {
+                    Console.WriteLine("i value: {0}, j value: {1}", i, j);
+                }
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}

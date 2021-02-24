3111b340c7b395a080bf1dec62fc4b02107f4419
Katelynn <10828878@my.uvu.edu>
2021-02-24 10:00:18 -0700

    While Loop
    Loops practice #2

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 4)
+            {
+                Console.WriteLine("i value: {0}", i);
+                i++;
+                if (i == 2)
+                    break;
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}

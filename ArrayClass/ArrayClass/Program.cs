41470570aa8e0b7592652b52d9812c59f661e0fa
Katelynn <10828878@my.uvu.edu>
2021-02-26 13:13:24 -0700

    Array Class
    Array and foreach loop practice

---
+++ ArrayClass/ArrayClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] {1, 4, 2, 3, 5};
+            Console.WriteLine("---Initial Array Elements---");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Array.Sort(array);
+            Console.WriteLine("---Elements After Sort---");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Array.Reverse(array);
+            Console.WriteLine("---Elements After Reverse---");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+        }
+    }
+}

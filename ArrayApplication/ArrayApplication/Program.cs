ffb6b6f6519d7152e6c52b37c441d670d12f5f67
Katelynn <10828878@my.uvu.edu>
2021-02-25 18:07:01 -0700

    Array
    Array + foreach practice

---
+++ ArrayApplication/ArrayApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int [10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            foreach (int j in n)
+            {
+                int i = j - 100;
+                Console.WriteLine("Element[{0}] = {1}", i, j);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

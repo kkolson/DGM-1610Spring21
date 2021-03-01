c23a9ed56d37106a748a9ef0f5ab98044b275b91
Katelynn <10828878@my.uvu.edu>
2021-02-28 19:34:08 -0700

    DoWhile
    Loop practice

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("i value: {0}", i);
+                i++;
+                if (i == 2)
+                    break;
+            } while (i < 4);
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}

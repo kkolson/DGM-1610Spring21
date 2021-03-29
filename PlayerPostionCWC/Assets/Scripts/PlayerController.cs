11a5277adb1e2fd2c7c741a703a378933f719a3b
Katelynn <10828878@my.uvu.edu>
2021-03-29 10:36:04 -0600

    PlayerPositionCWC - controller
    finished/cleaned up player controller and set bounds

--- PlayerPostionCWC/Assets/Scripts/PlayerController.cs
+++ PlayerPostionCWC/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,28 @@ public class PlayerController : MonoBehaviour
 
 {
     public float horizontalInput;
+
+    public float speed = 10f;
+
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    //keep the player in bounds
     void Update()
     {
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate((Vector3.right * (horizontalInput * Time.deltaTime * speed)));
     }
 }

9a9a0644098c689d847f7793e4a88282bbfeac0d
Katelynn <10828878@my.uvu.edu>
2021-04-10 16:33:45 -0600

    Lesson 3.1 - Jump and Run
    Move left script + components

---
+++ Lesson 3.1/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+   
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * (Time.deltaTime * speed));
+    }
+}

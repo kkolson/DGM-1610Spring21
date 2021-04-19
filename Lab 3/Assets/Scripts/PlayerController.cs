24b73da7fe76af682c456712875ee5348d1a71f0
Katelynn <10828878@my.uvu.edu>
2021-04-19 13:44:37 -0600

    Lab 3
    Edited scripts

--- Lab 3/Assets/Scripts/PlayerController.cs
+++ Lab 3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    private float zBound = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         
         
         playerRb.AddForce(Vector3.forward * (speed * verticalInput));
         playerRb.AddForce(Vector3.right * (speed * horizontalInput));
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }

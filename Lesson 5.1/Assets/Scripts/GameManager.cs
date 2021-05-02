cb297d056432adce5e51f97c62e31b0ff13ea832
Katelynn <10828878@my.uvu.edu>
2021-05-02 17:08:42 -0600

    Lesson 5.3

--- Lesson 5.1/Assets/Scripts/GameManager.cs
+++ Lesson 5.1/Assets/Scripts/GameManager.cs
@@ -9,22 +9,25 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        isGameActive = true;
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             
-
             Instantiate(targets[index]);
         }
         
@@ -40,5 +43,11 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
--- Lesson 5.1/Assets/Scripts/Target.cs
+++ Lesson 5.1/Assets/Scripts/Target.cs
@@ -44,14 +44,22 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        _gameManager.UpdateScore(pointValue);
+        
+        if (_gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            _gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            _gameManager.GameOver();
+        }
     }
 
     // Update is called once per frame
